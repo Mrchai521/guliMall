951de6ef84ab9c466c44c8c4b07fd2b89f2bd344
Mrchai521 <18636902115@163.com>
2021-03-09 13:56:56 +0800

    修改角色实体类

--- renrenfast/src/main/java/com/cxf/gulimall/renren/modules/sys/entity/SysRoleEntity.java
+++ renrenfast/src/main/java/com/cxf/gulimall/renren/modules/sys/entity/SysRoleEntity.java
@@ -1,8 +1,8 @@
 /**
  * Copyright (c) 2016-2019 人人开源 All rights reserved.
- *
+ * <p>
  * https://www.renren.io
- *
+ * <p>
  * 版权所有，侵权必究！
  */
 
@@ -26,37 +26,69 @@ import java.util.List;
 @Data
 @TableName("sys_role")
 public class SysRoleEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
-	
-	/**
-	 * 角色ID
-	 */
-	@TableId
-	private Long roleId;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 角色名称
-	 */
-	@NotBlank(message="角色名称不能为空")
-	private String roleName;
+    /**
+     * 角色ID
+     */
+    @TableId
+    private Long roleId;
 
-	/**
-	 * 备注
-	 */
-	private String remark;
-	
-	/**
-	 * 创建者ID
-	 */
-	private Long createUserId;
+    /**
+     * 角色名称
+     */
+    @NotBlank(message = "角色名称不能为空")
+    private String roleName;
+    /**
+     * 角色权限
+     */
+    private String roleKey;
 
-	@TableField(exist=false)
-	private List<Long> menuIdList;
-	
-	/**
-	 * 创建时间
-	 */
-	private Date createTime;
+    /**
+     * 角色排序
+     */
+    private String roleSort;
+    /**
+     * 数据范围（1：所有数据权限；2：自定义数据权限；3：本部门数据权限；4：本部门及以下数据权限）
+     */
+    private String dataScope;
+    /**
+     * 备注
+     */
+    private String remark;
 
-	
+    /**
+     * 创建者ID
+     */
+    private Long createUserId;
+
+    /**
+     * 角色状态（0正常 1停用）
+     */
+    private String status;
+
+    /**
+     * 删除标志（0代表存在 2代表删除）
+     */
+    private String delFlag;
+
+    @TableField(exist = false)
+    private List<Long> menuIdList;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+    /**
+     * 创建者
+     */
+    private String createBy;
+    /**
+     * 修改者
+     */
+    private String updateBy;
 }
